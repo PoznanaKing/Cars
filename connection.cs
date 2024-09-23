4ea3ce0a65e515ed0e8ec3aba4dde724d4f7d32e
PoznanaKing <pozsgain@kkszki.hu>
2024-09-23 11:14:38 +0200

    Connection class is avaliable.

---
+++ connection.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConsole
+{
+    public class Connection
+    {
+        public MySqlConnection connection;
+        public string Host;
+        public string Database;
+        public string Password;
+        public string Username;
+        public string ConnectionString;
+        public Connection()
+        {
+            Host = "localhost";
+            Password = "";
+            Database = "cars";
+            Username = "root";
+
+            this.ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+            
+            connection = new MySqlConnection(this.ConnectionString);
+        }
+
+
+    }
+}
