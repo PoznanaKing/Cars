f04111e94ed51fbb4c27a80cbfe074cf0766fc19
PoznanaKing <pozsgain@kkszki.hu>
2024-09-23 10:41:15 +0200

    The Car class is now avalibe.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CarConsole
 {
+
+    public class Car
+    {
+        private int Id { get; set; }
+        private string Brand { get; set; }
+        private string Type { get; set; }
+        private string License_Number { get; set; }
+        private DateTime ReleaseDate { get; set; }  // Új DateTime tulajdonság
+
+        // Getter és setter metódusok
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public string GetBrand()
+        {
+            return Brand;
+        }
+
+        public void SetBrand(string brand)
+        {
+            Brand = brand;
+        }
+
+        public string GetType()
+        {
+            return Type;
+        }
+
+        public void SetType(string type)
+        {
+            Type = type;
+        }
+
+        public string GetLicenseNumber()
+        {
+            return License_Number;
+        }
+
+        public void SetLicenseNumber(string licenseNumber)
+        {
+            License_Number = licenseNumber;
+        }
+
+        // Getter és setter a ReleaseDate-hez
+        public DateTime GetReleaseDate()
+        {
+            return ReleaseDate;
+        }
+
+        public void SetReleaseDate(DateTime releaseDate)
+        {
+            ReleaseDate = releaseDate;
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            
 
         }
     }
