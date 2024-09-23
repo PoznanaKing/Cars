58ee5a69ab7932e7cdd353cf2a47a296425b2aaa
PoznanaKing <pozsgain@kkszki.hu>
2024-09-23 11:25:15 +0200

    Car class is ready to use.

---
+++ Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConsole
+{
+    internal class Car
+    {
+        private int Id { get; set; }
+        private string Brand { get; set; }
+        private string Type { get; set; }
+        private string License_Number { get; set; }
+        private DateTime ReleaseDate { get; set; }  // Új DateTime tulajdonság
+
+        // Getter és setter metódusok
+        public int GetId()
+        {
+            return Id;
+        }
+
+        public void SetId(int id)
+        {
+            Id = id;
+        }
+
+        public string GetBrand()
+        {
+            return Brand;
+        }
+
+        public void SetBrand(string brand)
+        {
+            Brand = brand;
+        }
+
+        public string GetType()
+        {
+            return Type;
+        }
+
+        public void SetType(string type)
+        {
+            Type = type;
+        }
+
+        public string GetLicenseNumber()
+        {
+            return License_Number;
+        }
+
+        public void SetLicenseNumber(string licenseNumber)
+        {
+            License_Number = licenseNumber;
+        }
+
+        // Getter és setter a ReleaseDate-hez
+        public DateTime GetReleaseDate()
+        {
+            return ReleaseDate;
+        }
+
+        public void SetReleaseDate(DateTime releaseDate)
+        {
+            ReleaseDate = releaseDate;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -9,70 +10,16 @@ using System.Threading.Tasks;
 namespace CarConsole
 {
 
-    public class Car
-    {
-        private int Id { get; set; }
-        private string Brand { get; set; }
-        private string Type { get; set; }
-        private string License_Number { get; set; }
-        private DateTime ReleaseDate { get; set; }  // Új DateTime tulajdonság
-
-        // Getter és setter metódusok
-        public int GetId()
-        {
-            return Id;
-        }
-
-        public void SetId(int id)
-        {
-            Id = id;
-        }
-
-        public string GetBrand()
-        {
-            return Brand;
-        }
-
-        public void SetBrand(string brand)
-        {
-            Brand = brand;
-        }
-
-        public string GetType()
-        {
-            return Type;
-        }
-
-        public void SetType(string type)
-        {
-            Type = type;
-        }
-
-        public string GetLicenseNumber()
-        {
-            return License_Number;
-        }
-
-        public void SetLicenseNumber(string licenseNumber)
-        {
-            License_Number = licenseNumber;
-        }
-
-        // Getter és setter a ReleaseDate-hez
-        public DateTime GetReleaseDate()
-        {
-            return ReleaseDate;
-        }
-
-        public void SetReleaseDate(DateTime releaseDate)
-        {
-            ReleaseDate = releaseDate;
-        }
-    }
+    
 
 
     internal class Program
     {
+        public static List<Car> carList = new List<Car>();
+        public static void Feladat1()
+        {
+            
+        }
         static void Main(string[] args)
         {
             
